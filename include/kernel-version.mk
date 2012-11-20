177447cee12626503daadcaa5b777f1ea0d10857
Rui Shen <shenrui01@gmail.com>
2012-11-20 09:43:02 +0800

    init

---
+++ include/kernel-version.mk
@@ -0,0 +1,24 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+ifeq ($(LINUX_VERSION),3.3.8)
+  LINUX_KERNEL_MD5SUM:=f1058f64eed085deb44f10cee8541d50
+endif
+ifeq ($(LINUX_VERSION),3.6.7)
+  LINUX_KERNEL_MD5SUM:=089209f655c8811db20d8b6ff37a8093
+endif
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM?=x
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+ifeq ($(firstword $(call split_version,$(KERNEL_BASE))),2)
+  KERNEL_PATCHVER=$(call merge_version,$(wordlist 1,3,$(call split_version,$(KERNEL_BASE))))
+else
+  KERNEL_PATCHVER=$(KERNEL)
+endif
+
